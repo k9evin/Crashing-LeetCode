ba71f39005f57128dbf045d08b6df79a904f046e
Kevin Pang <pangmingkai@gmail.com>
2022-01-29 01:30:32 -0500

    Time: 132 ms (15.41%), Space: 47.6 MB (5.79%) - LeetHub

---
+++ 23-merge-k-sorted-listsa-idsolution-btn-23-hrefhttps-labuladonggiteeio-plugin-v3-qno23-and-amp-targetgitee-and-amp-1643437041083-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-23-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/23-merge-k-sorted-listsa-idsolution-btn-23-hrefhttps-labuladonggiteeio-plugin-v3-qno23-and-amp-targetgitee-and-amp-1643437041083-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-23-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        if (lists.length == 1) {
+            return lists[0];
+        }
+        ListNode result = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            result = mergeTwoLists(result, lists[i]);
+        }
+        return result;
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy;
+        // Two pointers
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        
+        if (p1 == null) {
+            p.next = p2;
+        }
+        if (p2 == null) {
+            p.next = p1;
+        }
+        return dummy.next;
+    }
+}
