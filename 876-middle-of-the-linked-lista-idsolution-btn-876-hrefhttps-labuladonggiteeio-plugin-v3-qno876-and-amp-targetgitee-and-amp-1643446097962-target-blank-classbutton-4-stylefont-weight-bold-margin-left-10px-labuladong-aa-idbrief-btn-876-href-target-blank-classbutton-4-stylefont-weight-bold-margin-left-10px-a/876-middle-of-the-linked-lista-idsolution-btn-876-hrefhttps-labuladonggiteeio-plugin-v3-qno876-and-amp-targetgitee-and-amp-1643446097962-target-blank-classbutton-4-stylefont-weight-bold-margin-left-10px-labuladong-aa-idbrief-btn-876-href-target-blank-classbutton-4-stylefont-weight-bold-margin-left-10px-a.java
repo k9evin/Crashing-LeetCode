9b7820305bccb02429e91613d596b604ab6b91bc
Kevin Pang <pangmingkai@gmail.com>
2022-01-29 04:01:19 -0500

    Time: 0 ms (100.00%), Space: 41.7 MB (8.86%) - LeetHub

--- 876-middle-of-the-linked-lista-idsolution-btn-876-hrefhttps-labuladonggiteeio-plugin-v3-qno876-and-amp-targetgitee-and-amp-1643446097962-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-876-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/876-middle-of-the-linked-lista-idsolution-btn-876-hrefhttps-labuladonggiteeio-plugin-v3-qno876-and-amp-targetgitee-and-amp-1643446097962-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-876-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 876-middle-of-the-linked-lista-idsolution-btn-876-hrefhttps-labuladonggiteeio-plugin-v3-qno876-and-amp-targetgitee-and-amp-1643446097962-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-876-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/876-middle-of-the-linked-lista-idsolution-btn-876-hrefhttps-labuladonggiteeio-plugin-v3-qno876-and-amp-targetgitee-and-amp-1643446097962-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-876-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -11,6 +11,8 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head, fast = head;
+        // 使用快慢指针，快指针走两步，慢指针走一步
+        // 当快指针到末尾时，慢指针在链表中间
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
