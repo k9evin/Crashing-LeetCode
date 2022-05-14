03bb590ee6aebe1dbee5ca58e40beda46ae691e0
Kevin Pang <pangmingkai@gmail.com>
2022-05-14 17:40:55 -0400

    Time: 4 ms (97.32%), Space: 53.7 MB (93.42%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        // If fast is not null, the linked list has odd number of nodes
+        if (fast != null) {
+            slow = slow.next;
+        }
+        
+        ListNode left = head;
+        ListNode right = reverse(slow); // Reverse the back half of the list
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        
+        return true;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
