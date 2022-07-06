dec8a0545fca59f4bdf7aa086cb5cca4807bf9a5
Kevin Pang <pangmingkai@gmail.com>
2022-07-05 22:07:04 -0700

    Time: 489 ms (85.55%), Space: 26 MB (72.32%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,6 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # 1. Brute force solution:
+        # Solution 1:
         # Time complexity: O(n^2)
         # Space complexity: O(1)
         # for i in range(0, len(nums)):
@@ -9,12 +9,23 @@ class Solution:
         #             return True;
         # return False;
         
-        # 2. Order the list:
-        # Time complexity: O(n^2)
+        # Solution 2:
+        # Time complexity: O(nlogn)
         # Space complexity: O(1)
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i - 1]:
+        #         return True
+        # return False
+        
+        # Solution 3:
+        # Time complexity: O()
+        # Space complexity: O()
+        hashset = set()        
+        for i in nums:
+            if i in hashset:
                 return True
+            hashset.add(i)
         return False
+        
             
