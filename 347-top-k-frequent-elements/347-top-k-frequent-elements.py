ffe1c82be82e165beeed5c53d4117eab9c2b5494
Kevin Pang <pangmingkai@gmail.com>
2022-07-06 22:16:19 -0700

    Time: 142 ms (62.75%), Space: 19.7 MB (14.55%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,16 +3,28 @@ class Solution:
         # Solution 1:
         # Time complexity: O(nlogn)
         # Space complexity: O(n)
-        freq = {}
-        for i in nums:
-            freq[i] = freq.get(i, 0) + 1
-        sorted_freq = dict(sorted(freq.items(), key=lambda x:x[1], reverse=True))
-        return list(sorted_freq.keys())[:k]
+        # freq = {}
+        # for i in nums:
+        #     freq[i] = freq.get(i, 0) + 1
+        # sorted_freq = dict(sorted(freq.items(), key=lambda x:x[1], reverse=True))
+        # return list(sorted_freq.keys())[:k]
     
 		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        for n, f in count.items():
+            freq[f].append(n)
+        result = []
+        for elem in range(len(freq) - 1, 0, -1):
+            for i in freq[elem]:
+                result.append(i)
+                if len(result) == k:
+                    return result
+        
 		# Solution 3:
         # Time complexity: O()
         # Space complexity: O()
