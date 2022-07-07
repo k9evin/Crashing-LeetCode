e5e53a84df43848acba34c338db1e9be63586bfd
Kevin Pang <pangmingkai@gmail.com>
2022-07-06 18:03:49 -0700

    Time: 118 ms (81.81%), Space: 19 MB (35.09%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,19 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # n: number of words, m: length of words
+        
         # Solution 1:
-        # Time complexity: O(m * nlogn) 
-        # m: number of words, n: length of words
-        # Space complexity: O(mn)
-        results = defaultdict(list)
-        for word in strs:
-            results["".join(sorted(word))].append(word)
-        return list(results.values())
+        # Time complexity: O(n*mlogm) 
+        # Space complexity: O(nm)
+        # results = defaultdict(list)
+        # for word in strs:
+        #     results["".join(sorted(word))].append(word)
+        # return list(results.values())
 		
 		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-        
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
+        # Time complexity: O(nm)
+        # Space complexity: O(n)
+        results = defaultdict(list)
+        for word in strs:
+            freq = [0] * 26
+            for char in word:
+                freq[ord(char) - ord('a')] += 1
+            results[tuple(freq)].append(word)
+        return list(results.values())
