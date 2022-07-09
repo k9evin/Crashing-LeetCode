0104a61ef7663ed2dffe26680aaaf8540f1e15f4
Kevin Pang <pangmingkai@gmail.com>
2022-07-09 00:25:36 -0700

    Time: 54 ms (83.43%), Space: 14.4 MB (85.73%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,22 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Solution 1:
-        # Time complexity: O()
-        # Space complexity: O()
-        string = "".join(filter(str.isalnum, s)).upper()
-        left, right = 0, len(string) - 1
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        # string = "".join(filter(str.isalnum, s)).upper()
+        # left, right = 0, len(string) - 1
+        # while left < right:
+        #     if string[left] != string[right]:
+        #         return False
+        #     left += 1
+        #     right -= 1
+        # return True
+		
+		# Solution 2:
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        left, right = 0, len(s) - 1
         while left < right:
-            if string[left] != string[right]:
+            # skip non-alphanumeric characters
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
             left += 1
             right -= 1
         return True
-        
-		
-		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
