1379570aa060b4528bd6dc23feb88f075738973b
Kevin Pang <pangmingkai@gmail.com>
2022-07-09 20:50:21 -0700

    Time: 789 ms (88.50%), Space: 18.3 MB (22.85%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -9,6 +9,8 @@ class Solution:
         for i, n in enumerate(nums):
             if i > 0 and n == nums[i - 1]:
                 continue
+            if n > 0:
+                break
             left, right = i + 1, len(nums) - 1
             while left < right:
                 result = n + nums[left] + nums[right]
