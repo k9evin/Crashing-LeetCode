c3cfbafeacacab237d63390fd6ba66b6ff8d54aa
Kevin Pang <pangmingkai@gmail.com>
2022-07-10 00:27:53 -0700

    Time: 1542 ms (8.37%), Space: 27.4 MB (54.64%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # Solution 1:
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        max_area = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            max_area = max(area, max_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
