ed0f7180a96344b87c523a297429a703d7747a98
Kevin Pang <pangmingkai@gmail.com>
2022-07-12 01:15:32 -0700

    Time: 143 ms (84.77%), Space: 14.1 MB (19.61%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -3,22 +3,32 @@ class Solution:
         # Solution 1:
         # Time complexity: O(26 * n)
         # Space complexity: O(n)
-        l = 0
-        res = 0
-        count = {}
-        
-        for r, char in enumerate(s):
-            count[char] = 1 + count.get(char, 0)
-            while ((r - l + 1) - max(count.values())) > k:
-                count[s[l]] -= 1
-                l += 1
-            res = max(res, (r - l + 1))
-        return res
+        # l = 0
+        # res = 0
+        # count = {}
+        # for r, char in enumerate(s):
+        #     count[char] = 1 + count.get(char, 0)
+        #     while ((r - l + 1) - max(count.values())) > k:
+        #         count[s[l]] -= 1
+        #         l += 1
+        #     res = max(res, (r - l + 1))
+        # return res
 		
 		# Solution 2:
         # Time complexity: O()
         # Space complexity: O()
+        l = 0
+        res = 0
+        max_freq = 0
+        count = {}
+        
+        for r, char in enumerate(s):
+            count[char] = 1 + count.get(char, 0)
+            max_freq = max(max_freq, count[char])
+            while ((r - l + 1) - max_freq) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, (r - l + 1))
+        
+        return res
 		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
