c0c4cd2d5d506ffd768ff63a1b1fe5f38ae1142c
Kevin Pang <pangmingkai@gmail.com>
2022-07-11 20:54:33 -0700

    Time: 66 ms (63.89%), Space: 13.8 MB (89.79%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -4,20 +4,21 @@ class Solution:
         # Time complexity: O(q * (m + n) + m * n) 
         # q is the length of the indices
         # Space complexity: O(m * n)
-        matrix = [[0] * n for _ in range(m)]
-        for i, j in indices:
-            # update the row
-            for c in range(n):
-                matrix[i][c] += 1
-            # update the column
-            for r in range(m):
-                matrix[r][j] += 1
-        return sum(x % 2 for row in matrix for x in row)
+        # matrix = [[0] * n for _ in range(m)]
+        # for i, j in indices:
+        #     # update the row
+        #     for c in range(n):
+        #         matrix[i][c] += 1
+        #     # update the column
+        #     for r in range(m):
+        #         matrix[r][j] += 1
+        # return sum(x % 2 for row in matrix for x in row)
 		
 		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
+        # Time complexity: O(q + m * n)
+        # Space complexity: O(m + n)
+        rows, cols = [0] * m, [0] * n
+        for i, j in indices:
+            rows[i] += 1
+            cols[j] += 1
+        return sum((r + c) % 2 for r in rows for c in cols)
