093ade6e2f6bbf3874c3b5aa185b674aef97f020
Kevin Pang <pangmingkai@gmail.com>
2022-07-11 18:59:32 -0700

    Time: 2201 ms (8.82%), Space: 25.1 MB (6.62%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Solution 1:
-        # Time complexity: O(n^2)
+        # Time complexity: O(n)
         # Space complexity: O(1)
         l, r, ans = 0, 1, 0
         while r < len(prices):
@@ -14,12 +14,3 @@ class Solution:
                 l = r
             r += 1
         return ans
-                
-		
-		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
