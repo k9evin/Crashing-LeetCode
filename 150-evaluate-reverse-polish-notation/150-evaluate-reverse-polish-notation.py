6f74dcc66c1957c7c36501e18a30167da8f82238
Kevin Pang <pangmingkai@gmail.com>
2022-07-15 03:29:21 -0700

    Time: 63 ms (98.40%), Space: 14.4 MB (56.91%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -3,26 +3,47 @@ class Solution:
         # Solution 1:
         # Time complexity: O(n)
         # Space complexity: O(n)
-        stack = []
-        res = 0
+#         stack = []
+#         res = 0
         
-        for i in tokens:
-            try:
-                stack.append(int(i))
-            except ValueError as e:
-                a = stack.pop()
-                b = stack.pop()
-                match i:
-                    case "+":
-                        res = a + b
-                    case "-":
-                        res = b - a
-                    case "*":
-                        res = a * b
-                    case "/":
-                        res = int(b / a)
-                stack.append(res)
-        return stack[-1]
+#         for i in tokens:
+#             try:
+#                 stack.append(int(i))
+#             except ValueError as e:
+#                 a = stack.pop()
+#                 b = stack.pop()
+#                 match i:
+#                     case "+":
+#                         res = a + b
+#                     case "-":
+#                         # the order matters!
+#                         res = b - a
+#                     case "*":
+#                         res = a * b
+#                     case "/":
+#                         # be careful with the division operator
+#                         res = int(b / a)
+#                 stack.append(res)
+#         return stack[-1]
+    
+        # Solution 2:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
                     
                         
                 
