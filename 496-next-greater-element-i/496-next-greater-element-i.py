d7baa319af3a315bdca2514d8f024ac76dbbf673
Kevin Pang <pangmingkai@gmail.com>
2022-07-15 22:57:39 -0700

    Time: 81 ms (58.29%), Space: 14.2 MB (14.03%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -3,28 +3,55 @@ class Solution:
         # Solution 1:
         # Time complexity: O(m + n)
         # Space complexity: O(n)
-        stack = []
-        res = []
-        dict = {}
+#         stack = []    # storing the index
+#         res = []
+#         dict = {}
+        
+#         for i, n in enumerate(nums2):
+#             # monotonic decreasing stack, if the curr number is >
+#             # than the one at the top of the stack, pop it out
+#             while stack and n > nums2[stack[-1]]:
+#                 idx = stack.pop()
+#                 # record the value and its corresponding next larger value
+#                 dict[nums2[idx]] = n
+#             stack.append(i)
+            
+#         for j in nums1:
+#             # find the correspond larger value through dictionary
+#             res.append(dict.get(j, -1))
+#         return res
+		
+		# Solution 2:
+        # Time complexity: O(m * n)
+        # Space complexity: O(m)
+#         dict = {n : i for i, n in enumerate(nums1)}
+#         res = [-1] * len(nums1)
+        
+#         for i in range(len(nums2)):
+#             if nums2[i] not in nums1:
+#                 continue
+#             for j in range(i + 1, len(nums2)):
+#                 if nums2[j] > nums2[i]:
+#                     idx = dict[nums2[i]]
+#                     res[idx] = nums2[j]
+#                     break
+#         return res
+            
+		
+		# Solution 3:
+        # Time complexity: O(m + n)
+        # Space complexity: O(m)
+        stack = []    # storing the value
+        res = [-1] * len(nums1)
+        dict = {n : i for i, n in enumerate(nums1)}
         
         for i, n in enumerate(nums2):
             # monotonic decreasing stack, if the curr number is >
             # than the one at the top of the stack, pop it out
-            while stack and n > nums2[stack[-1]]:
-                idx = stack.pop()
-                # record the value and its corresponding next larger value
-                dict[nums2[idx]] = n
-            stack.append(i)
-            
-        for j in nums1:
-            # find the correspond larger value through dictionary
-            res.append(dict.get(j, -1))
+            while stack and n > stack[-1]:
+                val = stack.pop()
+                idx = dict[val]
+                res[idx] = n
+            if n in nums1:
+                stack.append(n)
         return res
-		
-		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
