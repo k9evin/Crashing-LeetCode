e8c67b92273f5d3a392cb7a0f9bd4c2f43b03fa3
Kevin Pang <pangmingkai@gmail.com>
2022-07-16 00:25:33 -0700

    Time: 237 ms (85.97%), Space: 15.6 MB (84.10%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -8,19 +8,14 @@ class Solution:
         res = [-1] * n
         val = 0
         
+        # 用 n * 2 将数组长度翻倍，再用 i % n 来代表index
         for i in range(n * 2):
             num = nums[i % n]
             # monotonic decreasing stack (storing index)
             while stack and num > nums[stack[-1]]:
                 idx = stack.pop()
                 res[idx] = num
+            # append the index of the number, so later when we pop
+            # we know the corresponded result index
             stack.append(i % n)
         return res
-
-		# Solution 2:
-        # Time complexity: O()
-        # Space complexity: O()
-		
-		# Solution 3:
-        # Time complexity: O()
-        # Space complexity: O()
