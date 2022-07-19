882125346d67f2bc71370bf6b0a7b25fd021160f
Kevin Pang <pangmingkai@gmail.com>
2022-07-18 17:14:42 -0700

    Time: 253 ms (92.57%), Space: 15.5 MB (72.10%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,6 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i, n in enumerate(nums):
-            if n == target:
-                return i
+        # Solution 1:
+        # Time complexity: O(logn)
+        # Space complexity: O(1)
+        l = 0
+        r = len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if target < nums[mid]:
+                r = mid - 1
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                return mid
         return -1
