cf5962f50775e204b0c1a4ca3f29b8a5ffd5fac9
Kevin Pang <pangmingkai@gmail.com>
2022-07-27 18:20:03 -0700

    Time: 50 ms (64.30%), Space: 15.4 MB (55.79%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -8,22 +8,22 @@ class Solution:
         # Solution 1: iteratively
         # Time complexity: O(n)
         # Space complexity: O(1)
-        # prev, curr = None, head
-        # while curr:
-        #     nxt = curr.next
-        #     curr.next = prev
-        #     prev = curr
-        #     curr = nxt
-        # return prev
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
 		
 		# Solution 2: recursively
-        # Time complexity: O()
-        # Space complexity: O()
+        # Time complexity: O(n)
+        # Space complexity: O(n)
         # base case
-        if not head or not head.next:
-            return head
-        newHead = self.reverseList(head.next)
+        # if not head or not head.next:
+        #     return head
+        # newHead = self.reverseList(head.next)
         # connect with its prev element
-        head.next.next = head
-        head.next = None
-        return newHead
+        # head.next.next = head
+        # head.next = None
+        # return newHead
