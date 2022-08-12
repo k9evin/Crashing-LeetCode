d8d96a00480e8b6076e8e7636d9085798376038d
Kevin Pang <pangmingkai@gmail.com>
2022-08-12 01:09:11 -0700

    Time: 82 ms (78.59%), Space: 13.9 MB (43.89%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Solution 1:
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        dummy = ListNode(0)
+        p1, p2 = l1, l2
+        p = dummy
+        
+        carry = 0
+        while p1 or p2 or carry > 0:
+            value = carry
+            
+            if p1:
+                value += p1.val
+                p1 = p1.next
+            
+            if p2:
+                value += p2.val
+                p2 = p2.next
+            
+            carry = value // 10
+            value = value % 10
+            
+            p.next = ListNode(value)
+            p = p.next
+            
+        return dummy.next
+        
