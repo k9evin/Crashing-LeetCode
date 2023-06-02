c74d98a93b5a499b52f5ae0be3371c2b93dc3273
Mingkai Pang <pangmingkai@gmail.com>
2023-06-03 02:42:26 +0800

    Time: 160 ms (49.52%) | Memory: 17.2 MB (37.52%) - LeetSync

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -4,6 +4,7 @@ class Solution:
         left, right = 0, 1
         res = ''
         length = float("infinity")
+        satisfied = 0
 
         for i in t:
             t_dict[i] = t_dict.get(i, 0) + 1
@@ -11,14 +12,16 @@ class Solution:
         for right in range(len(s)):
             if s[right] in t_dict:
                 t_dict[s[right]] -= 1
-            while all(val <= 0 for val in t_dict.values()):
+                if t_dict[s[right]] == 0:
+                    satisfied += 1
+            while satisfied == len(t_dict):
                 if (right - left + 1) < length:
                     res = s[left: right + 1]
                     length = len(res)
                 if s[left] in t_dict:
+                    if t_dict[s[left]] == 0:
+                        satisfied -= 1
                     t_dict[s[left]] += 1
                 left += 1
-            #     print('in while loop')
-            # print(t_dict, res, len(res), length, s[left:right+1])
         return res
                 
