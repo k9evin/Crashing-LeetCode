997978582cc448dd0c417554de144ad327774728
Mingkai Pang <pangmingkai@gmail.com>
2023-08-05 07:21:17 +0800

    Time: 2243 ms (80.30%) | Memory: 80.5 MB (27.23%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -22,10 +22,6 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         def dfs(index: int, root: TrieNode) -> bool:
             curr = root
-            if not curr:
-                return False
-            if index == len(word):
-                return curr.isEnd
             for i in range(index, len(word)):
                 c = word[i]
                 if c == '.':
