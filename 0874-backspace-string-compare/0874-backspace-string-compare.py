d81c4e55f7edde85052f4578e6b0ebe78b092b88
Mingkai Pang <pangmingkai@gmail.com>
2023-10-19 16:49:23 -0700

    Time: 42 ms (33.77%), Space: 16.1 MB (83.41%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def backspaceCompare(self, S1, S2):
+        r1 = len(S1) - 1 
+        r2 = len (S2) - 1
+        
+        while r1 >= 0 or r2 >= 0:
+            char1 = char2 = ""
+            if r1 >= 0:
+                char1, r1 = self.getChar(S1, r1)
+            if r2 >= 0:
+                char2, r2 = self.getChar(S2, r2)
+            if char1 != char2:
+                return False
+        return True
+        
+    
+    def getChar(self, s , r):
+        char, count = '', 0
+        while r >= 0 and not char:
+            if s[r] == '#':
+                count += 1
+            elif count == 0:
+                char = s[r]
+            else:
+                count -= 1
+            r -= 1
+        return char, r
