22e994832c2bc1fafbe39bf4f028e70f234b20bb
Mingkai Pang <pangmingkai@gmail.com>
2023-10-26 16:14:31 -0700

    Time: 3281 ms (93.2%), Space: 18.8 MB (92.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -4,9 +4,10 @@ class Solution:
         n = len(nums)
         dp = [[0] * (n + 2) for _ in range(n + 2)]
         
-        for i in range(n, -1, -1):
+        for i in range(n + 1, -1, -1):
             for j in range(i + 1, n + 2):
                 for k in range(i + 1, j):
+                    
                     dp[i][j] = max(dp[i][j], 
                                    dp[i][k] + dp[k][j] + 
                                    points[i] * points[k] * points[j])
