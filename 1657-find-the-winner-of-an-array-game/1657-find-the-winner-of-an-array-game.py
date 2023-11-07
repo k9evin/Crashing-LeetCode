d9c46b27b8f80a98f23ceff06be78b8484b461eb
Mingkai Pang <pangmingkai@gmail.com>
2023-11-06 16:15:54 -0800

    Time: 527 ms (54.81%), Space: 30.2 MB (36.01%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -1,15 +1,19 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
+        # initialize the current winner as the first number
         curr_winner = arr[0]
         win = 0
 
         for n in arr[1:]:
+            # if the new number is larger, update the current winner and count
             if n > curr_winner:
                 curr_winner = n
                 win = 0
 
+            # increment the win count if it is the same number
             win += 1
-            
+
+            # break out of the loop if reach k
             if win == k:
                 break
 
