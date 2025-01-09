e7a8ef5e27688c1284f824c327833190b82f5fab
Mingkai Pang <pangmingkai@gmail.com>
2025-01-09 03:43:41 -0800

    Time: 359 ms (47.74%), Space: 17.9 MB (34.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        def palindrom(s, l, r):
-            # check if they are within bound and palindrom
+        res = ""
+        # 由中间向两边扩散
+        for i in range(len(s)):
+            # 奇数长度
+            l, r = i, i
+            # 确保l, r在范围内，而且这两个字符串相同才算palindrome
             while l >= 0 and r < len(s) and s[l] == s[r]:
+                # 字符串长度比现在res长，才更新res
+                if (r - l + 1) > len(res):
+                    res = s[l : r + 1]
                 l -= 1
                 r += 1
-            # when we are out of the loop, s[l] != s[r],
-            # so we need to use the previous l, r values
-            return s[l+1: r]
-
-        res = ''
-        for i in range(len(s)):
-            # if the length of str is odd
-            odd = palindrom(s, i, i)
-            # if the length of str is even
-            even = palindrom(s, i, i + 1)
-
-            # the longest str will be the result
-            res = max(res, odd, even, key=len)
-            
+            # 偶数长度
+            l, r = i, i + 1
+            # 确保l, r在范围内，而且这两个字符串相同才算palindrome
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                # 字符串长度比现在res长，才更新res
+                if (r - l + 1) > len(res):
+                    res = s[l : r + 1]
+                l -= 1
+                r += 1
         return res
