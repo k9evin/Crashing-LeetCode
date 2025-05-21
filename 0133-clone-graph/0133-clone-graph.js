dacec3de4b5c78f7a5a4232034b6f75ae0b5a1f5
Mingkai Pang <pangmingkai@gmail.com>
2025-05-21 00:30:25 -0700

    Time: 42 ms (91.48%), Space: 58.7 MB (6.36%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function (node) {
+    let visited = new Map();
+
+    function dfs(node) {
+        if (node == null) {
+            return node;
+        }
+        if (visited.has(node)) {
+            return visited.get(node);
+        }
+        let cloneNode = new _Node(node.val, []);
+        visited.set(node, cloneNode);
+
+        node.neighbors.forEach((neighbor) => {
+            cloneNode.neighbors.push(dfs(neighbor));
+        });
+        return cloneNode;
+    }
+
+    return dfs(node);
+};
