8a697d96150af83cce033c71c19f41d927029055
Mingkai Pang <pangmingkai@gmail.com>
2025-05-24 00:38:57 -0700

    Time: 0 ms (100%), Space: 57.2 MB (67.65%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function (n, edges) {
+    // A valid tree must have exactly n - 1 edges.
+    if (edges.length > n - 1) {
+        return false;
+    }
+
+    const adj = Array.from({ length: n }, () => []);
+    for (const [u, v] of edges) {
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+
+    const visited = new Set();
+    const dfs = (node, parent) => {
+        if (visited.has(node)) {
+            return false;
+        }
+        visited.add(node);
+        for (const neighbor of adj[node]) {
+            if (neighbor === parent) {
+                continue;
+            }
+            if (!dfs(neighbor, node)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return dfs(0, -1) && visited.size === n;
+};
