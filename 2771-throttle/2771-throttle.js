6ae0e07b371daf26d454ca0ff35d799490bc9216
Mingkai Pang <pangmingkai@gmail.com>
2025-06-24 13:09:09 -0700

    Time: 52 ms (65.6%), Space: 53.8 MB (58.4%) - LeetHub

---
+++ 2771-throttle/2771-throttle.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var throttle = function (fn, t) {
+    let timeoutId = null;
+    let nextTimeToCallFn = 0;
+
+    return function (...args) {
+        // update the delayed time to call the previous function
+        const delay = Math.max(0, nextTimeToCallFn - Date.now());
+
+        // clear the existing timeout to ensure only one timeout is running at a given time
+        clearTimeout(timeoutId);
+
+        // set a new timeout, invoke the function, and update the nextTimetoCallFn
+        timeoutId = setTimeout(() => {
+            fn(...args);
+            nextTimeToCallFn = Date.now() + t;
+        }, delay);
+    }
+};
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
