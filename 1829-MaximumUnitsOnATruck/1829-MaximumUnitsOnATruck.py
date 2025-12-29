158c6e06b61a729d4f640ddf24fb546207c8bb6d
Mingkai Pang <pangmingkai@gmail.com>
2025-12-29 01:41:05 -0500

    Create 1829-MaximumUnitsOnATruck

---
+++ 1829-MaximumUnitsOnATruck/1829-MaximumUnitsOnATruck.py
@@ -0,0 +1,21 @@
+# Last updated: 12/29/2025, 1:41:05 AM
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        # Solution 1:
+        # Time complexity: O(nlogn)
+        # Space complexity: O(1)
+        
+        # sort the array base on the units
+        boxTypes.sort(key=lambda x: x[1], reverse=True)
+        capacity = 0
+        
+        for box, unit in boxTypes:
+            if truckSize >= box:
+                capacity += box * unit
+                truckSize -= box
+            else:
+                capacity += truckSize * unit
+                break
+        
+        return capacity
+                
