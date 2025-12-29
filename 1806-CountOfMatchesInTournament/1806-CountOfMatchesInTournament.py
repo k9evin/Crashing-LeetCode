1215d4c627f816b8b6d87bd841ec1470a34b9236
Mingkai Pang <pangmingkai@gmail.com>
2025-12-29 01:41:09 -0500

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.py
@@ -0,0 +1,14 @@
+# Last updated: 12/29/2025, 1:41:09 AM
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        res = 0
+
+        while n > 1:
+            if n % 2 == 0:
+                res += n // 2
+                n = n // 2
+            else:
+                res += (n - 1) // 2
+                n = (n - 1) // 2 + 1
+            
+        return res
