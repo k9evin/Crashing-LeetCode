847305106a3eb2a644f5902023e052874791107f
Mingkai Pang <pangmingkai@gmail.com>
2025-12-29 01:40:44 -0500

    Create 2119-MinimumNumberOfOperationsToMakeArrayContinuous

---
+++ 2119-MinimumNumberOfOperationsToMakeArrayContinuous/2119-MinimumNumberOfOperationsToMakeArrayContinuous.py
@@ -0,0 +1,16 @@
+# Last updated: 12/29/2025, 1:40:44 AM
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n
+        new_nums = sorted(set(nums))
+        j = 0
+        
+        for i in range(len(new_nums)):
+            while j < len(new_nums) and new_nums[j] < new_nums[i] + n:
+                j += 1
+            
+            count = j - i
+            ans = min(ans, n - count)
+
+        return ans
