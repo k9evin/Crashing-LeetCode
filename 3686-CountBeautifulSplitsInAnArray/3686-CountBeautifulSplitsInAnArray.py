b92be78288342229935956d57b091eeaaf7b5c8d
Mingkai Pang <pangmingkai@gmail.com>
2025-12-29 01:40:08 -0500

    Create 3686-CountBeautifulSplitsInAnArray

---
+++ 3686-CountBeautifulSplitsInAnArray/3686-CountBeautifulSplitsInAnArray.py
@@ -0,0 +1,17 @@
+# Last updated: 12/29/2025, 1:40:08 AM
+class Solution:
+    def beautifulSplits(self, nums: List[int]) -> int:
+        n = len(nums)
+        # lcp[i][j] 表示 s[i:] 和 s[j:] 的最长公共前缀
+        lcp = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, i - 1, -1):
+                if nums[i] == nums[j]:
+                    lcp[i][j] = lcp[i + 1][j + 1] + 1
+
+        ans = 0
+        for i in range(1, n - 1):
+            for j in range(i + 1, n):
+                if i <= j - i and lcp[0][i] >= i or lcp[i][j] >= j - i:
+                    ans += 1
+        return ans
