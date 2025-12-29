4d1f82187b9f7cfe6d4876f61f6a4599e0e93ab5
Mingkai Pang <pangmingkai@gmail.com>
2025-12-29 01:40:55 -0500

    Create 1956-MaximumElementAfterDecreasingAndRearranging

---
+++ 1956-MaximumElementAfterDecreasingAndRearranging/1956-MaximumElementAfterDecreasingAndRearranging.py
@@ -0,0 +1,14 @@
+# Last updated: 12/29/2025, 1:40:55 AM
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        n = len(arr)
+        counts = [0] * (n + 1)
+
+        for num in arr:
+            counts[min(num, n)] += 1
+
+        ans = 1
+        for num in range(2, n + 1):
+            ans = min(ans + counts[num], num)
+
+        return ans
