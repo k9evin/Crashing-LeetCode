aa685a03045c6c1f9632c3374893169e6f877e0a
Mingkai Pang <pangmingkai@gmail.com>
2026-01-10 23:53:06 -0500

    File updated at 1/10/2026, 11:53:06 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,17 +1,25 @@
-# Last updated: 1/10/2026, 11:39:22 PM
+# Last updated: 1/10/2026, 11:53:06 PM
 1class Solution:
 2    def findMin(self, nums: List[int]) -> int:
-3        l, r = 0, len(nums) - 1
-4
-5        if nums[0] <= nums[-1]:
-6            return nums[0]
-7
-8        while l < r:
-9            m = l + (r - l) // 2
-10            if nums[m] > nums[r]:
-11                l = m + 1
-12            else:
-13                r = m
-14
-15        return nums[l]
-16
+3        # Initialize pointers for binary search (closed interval [l, r])
+4        l, r = 0, len(nums) - 1
+5
+6        # Optimization: if array is not rotated (or has only one element),
+7        # the first element is the minimum
+8        if nums[0] <= nums[-1]:
+9            return nums[0]
+10
+11        # Binary search to find the rotation pivot (minimum element)
+12        while l < r:
+13            m = l + (r - l) // 2  # Prevents potential overflow
+14            
+15            # If mid element is greater than rightmost element,
+16            # the minimum must be in the right half (after mid)
+17            if nums[m] > nums[r]:
+18                l = m + 1
+19            # Otherwise, the minimum is in the left half (including mid)
+20            else:
+21                r = m
+22
+23        # When loop ends, l == r and points to the minimum element
+24        return nums[l]
