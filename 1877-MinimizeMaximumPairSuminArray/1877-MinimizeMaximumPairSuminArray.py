4cc47207c8fad6560ee7a839343fc8351f11dd49
Mingkai Pang <pangmingkai@gmail.com>
2026-01-25 19:46:27 -0500

    File updated at 1/25/2026, 7:46:27 PM

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.py
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.py
@@ -1,15 +1,15 @@
-# Last updated: 1/24/2026, 6:42:35 PM
+# Last updated: 1/25/2026, 7:46:27 PM
 1class Solution:
 2    def minPairSum(self, nums: List[int]) -> int:
 3        nums.sort()
-4        
-5        l, r = 0, len(nums) - 1
-6        max_sum = 0
-7
-8        while l < r:
-9            curr_sum = nums[l] + nums[r]
-10            max_sum = max(max_sum, curr_sum)
-11            l += 1
-12            r -= 1
-13
-14        return max_sum
+4        max_sum = -1
+5
+6        l, r = 0, len(nums) - 1
+7        while l < r:
+8            curr_sum = nums[l] + nums[r]
+9            max_sum = max(max_sum, curr_sum)
+10            l += 1
+11            r -= 1
+12
+13        return max_sum
+14
